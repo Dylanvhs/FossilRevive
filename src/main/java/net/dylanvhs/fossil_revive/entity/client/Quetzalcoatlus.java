67ebb44266558f02d5631a05c95234a94625a782
Dylanvhs <dylanvhs257@gmail.com>
2024-02-09 15:51:42 +0100

    going insane

--- src/main/java/net/dylanvhs/fossil_revive/entity/client/Quetzalcoatlus.java
+++ src/main/java/net/dylanvhs/fossil_revive/entity/client/Quetzalcoatlus.java
@@ -93,10 +93,10 @@ public class Quetzalcoatlus<T extends Entity> extends HierarchicalModel<T> {
 		this.applyHeadRotation(netHeadYaw, headPitch, ageInTicks);
 
 
-		this.animateWalk(ModAnimationDefinitions.QUETZALCOATLUS_WALK, limbSwing, limbSwingAmount, 2f, 2.5f);
+		this.animateWalk(ModAnimationDefinitions.QUETZALCOATLUS_FLY, limbSwing, limbSwingAmount, 2f, 2.5f);
 		this.animate(((QuetzalcoatlusEntity) entity).idleAnimationState, ModAnimationDefinitions.QUETZALCOATLUS_IDLE, ageInTicks, 1f);
 		this.animate(QuetzalcoatlusEntity.attackAnimationState, ModAnimationDefinitions.LIOPLEURODON_ATTACK, ageInTicks, 1f);
-		this.animate(((QuetzalcoatlusEntity) entity).flyAnimationState, ModAnimationDefinitions.QUETZALCOATLUS_FLY, ageInTicks, 1f);
+		this.animate(((QuetzalcoatlusEntity) entity).flyAnimationState, ModAnimationDefinitions.QUETZALCOATLUS_WALK, ageInTicks, 1f);
 	}
 
 	private void applyHeadRotation(float pNetHeadYaw, float pHeadPitch, float pAgeInTicks) {
